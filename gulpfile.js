089e1f47c7bcd0fce9e123a5a2a8bffc958f7334
Lasse Hove Nielsen <lassehovenielsen@Lasses-iMac.local>
2020-12-07 06:20:34 +0100

    updated gulp with sass

--- gulpfile.js
+++ gulpfile.js
@@ -4,8 +4,22 @@ const uglify = require( "gulp-uglify-es" ).default;
 const polyfiller = require( 'gulp-polyfiller' );
 const concat = require( 'gulp-concat' );
 const rename = require( 'gulp-rename' );
+const sass = require( 'gulp-sass' );
 
-gulp.task( "build", () => {
+function style() {
+	return (
+		gulp
+		.src( "src/**/*.scss" )
+		.pipe( sass({outputStyle: 'compressed'}) )
+		.on( "error", sass.logError )
+
+		.pipe( rename( 'customselect.css' ) )
+		.pipe( gulp.dest( "dist/" ) )
+	);
+}
+
+
+function js() {
 	return gulp.src( [ 'src/jquery.customselect.js' ] )
 		.pipe( babel() )
 		.pipe( polyfiller( [ 'Array.prototype.find', 'Array.from' ] ) )
@@ -14,4 +28,11 @@ gulp.task( "build", () => {
 		.pipe( uglify() )
 		.pipe( rename( 'jquery.customselect.min.js' ) )
 		.pipe( gulp.dest( 'dist/' ) );
-} );
+}
+
+function watch() {
+	gulp.watch( 'src/**/*.scss', style )
+	gulp.watch('src/jquery.customselect.js', js);
+}
+
+exports.watch = watch;
